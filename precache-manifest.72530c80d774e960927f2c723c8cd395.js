072b7b073d22292bd4485718a4880e67a49cf8a2
deepak1051 <deepakpanwar1051@gmail.com>
2023-01-27 16:52:24 +0530

    Updates

---
+++ precache-manifest.72530c80d774e960927f2c723c8cd395.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "787fd3595f01d09e7bf61e2e64d77027",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "66d7348c8ba831054437",
+    "url": "/quiz-app/static/css/main.9f977465.chunk.css"
+  },
+  {
+    "revision": "23392d701e52c9737fdd",
+    "url": "/quiz-app/static/js/2.fbcc0c68.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/quiz-app/static/js/2.fbcc0c68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66d7348c8ba831054437",
+    "url": "/quiz-app/static/js/main.f87c0e3d.chunk.js"
+  },
+  {
+    "revision": "7c500c1ed059a6937f43",
+    "url": "/quiz-app/static/js/runtime-main.5fdbb1c1.js"
+  }
+]);
